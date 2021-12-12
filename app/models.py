7d4a3dbc922b6415c6e59deb08fafd54e0dd7699
frappaaa <francescoluppi@aol.com>
2021-12-12 19:29:59 +0100

    DEL: eleminati file

--- app/models.py
+++ app/models.py
@@ -36,28 +36,3 @@ class Vote(Base):
         "users.id", ondelete="CASCADE"), primary_key=True)
     post_id = Column(Integer, ForeignKey(
         "posts.id", ondelete="CASCADE"), primary_key=True)
-
-class HealthCenters(Base):
-    __tablename__ = "health_centers"
-    id = Column(Integer, primary_key=True, nullable=False)
-    nome: Column(String, nullable=False)
-    struttura: Column(String, nullable=False)
-    indirizzo: Column(String, nullable=False)
-    telefono: Column(String, nullable=True)
-    email: Column(String, nullable=True)
-    sito: Column(String, nullable=True)
-    lat: Column(float, nullable=True)
-    lng: Column(float, nullable=True)
-    descrizione_breve: Column(String, nullable=True)
-    descrizione: Column(String, nullable=True)
-    orari: Column(String, nullable=True)
-    foto: Column(String, nullable=True)
-    foto_thumb: Column(String, nullable=True)
-    foto_thumb_2: Column(String, nullable=True)
-    published = Column(Boolean, server_default='TRUE', nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=text('now()'))
-    owner_id = Column(Integer, ForeignKey(
-        "users.id", ondelete="CASCADE"), nullable=False)
-
-    owner = relationship("User")
